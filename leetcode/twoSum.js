5ed7d89aaa66fca31d90c82e172826feaed3641b
YRASA <zzceaon@outlook.com>
2020-04-06 14:10:21 +0800

    modify twoSum.js

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -37,3 +37,16 @@ const twoSum = function(nums, target) {
     }
 };
 console.log(twoSum([2, 7, 11, 15], 9));
+
+// const twoSum = function(nums, target) {
+//     let obj = {};
+//     for (let i = 0; i < nums.length; i++) {
+//         const item = nums[i];
+//         if (obj[item] >= 0) {
+//             return [obj[item], i]
+//         }
+//         else {
+//             obj[target - item] = i;
+//         }
+//     }
+// };
