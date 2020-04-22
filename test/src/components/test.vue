acd0acd063f5b7fbd958685cd19c68ec2d227eeb
YRASA <zzceaon@outlook.com>
2020-04-23 01:02:10 +0800

    add test

---
+++ test/src/components/test.vue
@@ -0,0 +1,32 @@
+<template>
+<!-- 模板区域(DOM结构) -->
+    <h2 class="red">test...{{msg}}</h2>
+</template>
+
+<script>
+// 脚本区域
+    export default {
+        name: 'test',
+        props: {
+            msg: {
+                type: String,
+                default: " test msg"
+            }
+        },
+        methods:{
+
+        },
+        data() {
+            return {
+
+            }
+        },
+    }
+</script>
+
+<style>
+/* 样式区域 */
+    .red {
+        color: #ff0000;
+    }
+</style>
