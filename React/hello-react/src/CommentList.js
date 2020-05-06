772b8cbc4092d38a0acceb84c9b794b6e00c282c
Zzceaon <zzceaon@outlook.com>
2020-05-07 00:06:06 +0800

    React

---
+++ React/hello-react/src/CommentList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function CommentList() {
+    return (
+        <ul>
+            <li>用户名:********</li>
+            <li>用户名:********</li>
+            <li>用户名:********</li>
+        </ul>
+    )
+}
+export default CommentList;
---
+++ React/hello-react/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// 入口文件
+import CommentApp from './CommentApp';
+ReactDOM.render(
+    <CommentApp />,
+    document.getElementById('root')
+);
