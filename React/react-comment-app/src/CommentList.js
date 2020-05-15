ac2e8464ae7b6f053e1fa6748dd59aa103de5669
Zzceaon <zzceaon@outlook.com>
2020-05-15 11:22:52 +0800

    modify react-comment-app/src/CommentList.js

--- React/react-comment-app/src/CommentList.js
+++ React/react-comment-app/src/CommentList.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Comment from './Comment';
 class CommentList extends Component {
     render() {
+        // console.log(this.props, "++++++");
+        let { comments } = this.props; // 在下面不用this.props.comments
         return (
             <div>
-                CommentList
+                { comments.map((comment, i) => <Comment comment={comment} key={i}/>)}
             </div>
         )
     }
