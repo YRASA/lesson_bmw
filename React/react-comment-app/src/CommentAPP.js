09def7dc9313df99365e10f3fc7088b8a77b4be9
Zzceaon <zzceaon@outlook.com>
2020-05-15 11:22:15 +0800

    modify react-comment-app/src/CommentAPP.js

--- React/react-comment-app/src/CommentAPP.js
+++ React/react-comment-app/src/CommentAPP.js
@@ -4,16 +4,42 @@ import CommentList from './CommentList'
 // let { a } = {a:1, b:2, c:3}
 // react facebook 高级 OO
 class CommentAPP extends Component {
+    constructor() {
+        super();
+        this.state = {
+            comments: [{
+                username: 'zc',
+                content: 'sagajaouih'
+            }, {
+                username: 'cc',
+                content: 'asftatg'
+            }]
+        }
+    }
     render() { // 接口
+        const { comments } = this.state;
         return (
             <div className="wrapper">
+                {/* props */}
+                {/* react 组件 render 页面输出, state 内部数据 props 外部数据 */}
+                {/* this.handleSubmitComment指向CommentInput 加.bind(this)指向对象的this.state */}
                 <CommentInput onSubmit={this.handleSubmitComment.bind(this)}/>
-                <CommentList />
+                {/* 向父组件要什么? 1.静态页面 2.动态化的 state props 显示评论列表 */}
+                <CommentList comments={comments}/>
             </div>
         )
     }
     handleSubmitComment(comment) {
-        console.log(comment);
+        // console.log(comment);
+        // let { comments } = this.state; // oldState
+        // comments.unshift(comment); // newState
+        // this.setState({
+        //     comments: comments
+        // })
+        // console.log(comments, "++++++++");
+        this.setState({
+            comments: [comment, ...this.state.comments] // 展开运算符
+        })
     }
 }
 export default CommentAPP;
