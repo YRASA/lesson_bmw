ba046834c72862ce63c4c24b54be040b5947b8b1
Zzceaon <zzceaon@outlook.com>
2020-05-18 01:15:51 +0800

    modify cache-file/app.js

--- interview/status_code/cache-file/app.js
+++ interview/status_code/cache-file/app.js
@@ -7,14 +7,25 @@ let server = http.createServer((req, res) => {
     // 用Etag做缓存(类似版本号)
     // If-None-Match
     if (req.url == '/') {
-        res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
-        fs.createReadStream('index.html').pipe(res);
+        // res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+        fs.createReadStream('./index.html').pipe(res);
     } else if (req.url == '/users') {
-        res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+        // res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
         // 发送 文本 Buffer
         // JSON对象变成文本
         // res.setHeader('Etag', version);
-        res.end(JSON.stringify(users)); // 怎么把JSON传过去?
+        // res.end(JSON.stringify(users)); // 怎么把JSON传过去?
+        if (req.headers['if-none-match'] && Number(req.headers['if-none-match']) == version) {
+            res.statusCode = 304;
+            res.end();
+        } else {
+            res.setHeader('Etag', version);
+            res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+            res.end(JSON.stringify({
+                version,
+                users
+            }));
+        }
     }
     // if (req.headers['if-none-match']) {
     //     // console.log(Number(req.headers['if-none-match']) == version);
