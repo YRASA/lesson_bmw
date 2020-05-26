cf7d8a44482d111a23f122130e6aae22f7bdf3f6
Zzceaon <zzceaon@outlook.com>
2020-05-26 14:24:32 +0800

    add comment-book/src/CommentApp.js

---
+++ React/comment-book/src/Comment.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Comment extends Component {
+    constructor () {
+        super()
+    }
+
+    render () {
+        return (
+            <div className='comment'>
+                123
+            </div>
+        )
+    }
+}
+
+export default Comment
