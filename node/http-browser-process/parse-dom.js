fd22c155902931b07c4440cf1e3457ad7da81ba5
Zzceaon <zzceaon@outlook.com>
2020-06-01 20:45:11 +0800

    add parse-dom.js

--- node/http-browser-process/parse-dom.js
+++ node/http-browser-process/parse-dom.js
@@ -2,6 +2,8 @@ let htmlStr = `
 <html>
     <head></head>
     <body>
+        <img />
+        <span></span>
         <div></div>
     </body>
 </html>
@@ -11,9 +13,39 @@ let htmlStr = `
 // 正则 split(): 完整的 html
 // token: html tag
 let currentToken = null;
+let stack = [ { type: 'document', children: [] } ];
 parse(htmlStr);
+console.log(JSON.stringify(stack[0], null, 2));
 function emit(token) {
     console.log(token);
+    let top = stack[stack.length - 1];
+    if (token.type === 'startTag') {
+        // push pop 处理配对
+        let element = {
+            type: 'element',
+            children: [],
+            attributes: [],
+            tagName: token.tagName
+        }
+        stack.push(element);
+        // 作为栈顶的元素子节点, 为了生成树
+        // if (!top.children) top.children = [];
+        top.children.push(element);
+    } else if (token.type === 'endTag') {
+        if (token.tagName !== top.tagName) {
+            throw new Error('tagName match error')
+        } else {
+            stack.pop();
+        }
+    } else if (token.type === 'selfCloseToken') {
+        let element = {
+            type: 'element',
+            children: [],
+            attributes: [],
+            tagName: token.tagName
+        }
+        top.children.push(element);
+    }
     currentToken = null;
 }
 function parse(htmlString) {
@@ -47,10 +79,19 @@ function tagOpen(c) {
 function tagName(c) {
     if (c.match(/[a-zA-Z]/)) {
         currentToken.tagName += c;
+        return tagName;
+    } else if (c.match(/[\t\n\f ]/)) {
+        return beforeAttributeName;
     } else if (c === '>') {
         // tag 拼接结束
         emit(currentToken);
         return start;
+    }
+}
+function beforeAttributeName(c) {
+    if (c === '/') {
+        currentToken.type = 'selfCloseToken';
+        return tagName;
     }
 }
 function endTagOpen(c) {
