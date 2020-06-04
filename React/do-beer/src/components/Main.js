56fcd6d3afdb0d409d29dacd4c05bf9e2f1ff601
Zzceaon <zzceaon@outlook.com>
2020-06-05 03:40:22 +0800

    modify do-beer/src/components/Main.js

--- React/do-beer/src/components/Main.js
+++ React/do-beer/src/components/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // StatefullComponent StatelessComponent
 import Search from './Search';
+import Header from './Header';
 import Results from './Results';
 
 class Main extends React.Component {
@@ -19,9 +20,27 @@ class Main extends React.Component {
         const searchTerm = params.searchTerm || undefined;
         this.loadBeers(searchTerm);
     }
+    componentDidUpdate(prevProps) {
+        // console.log(prevProps);
+        const currentSearchTerm = this.props.match.params.searchTerm; // 新参数
+        const oldSearchTerm = prevProps.match.params.searchTerm;
+        // console.log(oldSearchTerm, currentSearchTerm);
+        if (currentSearchTerm !== oldSearchTerm) {
+            this.loadBeers(currentSearchTerm)
+        }
+    }
     // = "hops"
     // es6 赋默认值
     loadBeers(searchTerm = "hops") {
+        const localStorageBeers = localStorage.getItem(`search-${searchTerm}`);
+        if (localStorageBeers) {
+            const localBeers = JSON.parse(localStorageBeers);
+            this.setState({
+                beers: localBeers,
+                loading: false
+            })
+            return;
+        }
         fetch(`http://api.react.beer/v2/search?q=${searchTerm}&type=beer`) //api 地址
             .then(data => data.json())
             .then(data => {
@@ -30,6 +49,12 @@ class Main extends React.Component {
                     loading: false,
                     beers
                 });
+                // 业务
+                // 列表记录相关 searchTerm 变化
+                localStorage.setItem(
+                    `search-${searchTerm}`,
+                    JSON.stringify(this.state.beers)
+                )
                 console.log(data);
             })
     }
@@ -37,6 +62,7 @@ class Main extends React.Component {
     render() {
         return (
             <div>
+                <Header siteName="Beer me!" />
                 <Search />
                 <Results beers={this.state.beers} loading={this.state.loading} />
             </div>
