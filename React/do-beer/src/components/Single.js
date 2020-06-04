073d73a7add7a1b0ced42b48778628cf4ad8e6dd
Zzceaon <zzceaon@outlook.com>
2020-06-05 03:40:54 +0800

    modify do-beer/src/components/Single.js

--- React/do-beer/src/components/Single.js
+++ React/do-beer/src/components/Single.js
@@ -1,9 +1,56 @@
 import React from 'react';
+import Header from './Header';
+import PropTypes from 'prop-types'; // 校验所有的参数类型 props
 
+// 开发流程
 class Single extends React.Component {
+    static propTypes = {
+        params: PropTypes.object
+    }
+    constructor() {
+        super();
+        this.state = {
+            beer: {},
+            loading: true // 业务经验
+        }
+    }
+    componentDidMount() {
+        // console.log(this.props.match.params);
+        // console.log(`searching for ${this.props.match.params.beerId}`);
+        this.loadBeer(this.props.match.params.beerId);
+    }
+    loadBeer = (beerId) => {
+        this.setState({ loading: true });
+        fetch(`http://api.react.beer/v2/beer/${beerId}`)
+        .then(data => data.json())
+        .then(res => {
+            this.setState({
+                beer: res.data,
+                loading: false
+            })
+        })
+    }
     render() {
+        if (this.state.loading) {
+            return <div>Loading...</div>
+        }
+        const { beer } = this.state;
+        const image = beer.labels ? beer.labels.large : 'null.jpg';
         return (
-            <div>Single</div>
+            <div>
+                <Header siteName="Beer me!"></Header>
+                <div className="signle-beer">
+                    <div className="desc">
+                        <h2>{ beer.name }</h2>
+                        <p>{ beer.description }</p>
+                    </div>
+                    <img className="label" src={ image } alt={ beer.name } />
+                    <div className="style">
+                        <h3>More Info on { beer.style.name }</h3>
+                        <p>{ beer.style.description }</p>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
