73a01ad07f7c44052110d98ece9aa7ff708609fc
Zzceaon <zzceaon@outlook.com>
2020-06-14 20:57:55 +0800

    react-jianshu/src/App.js

--- React/react-jianshu/src/App.js
+++ React/react-jianshu/src/App.js
@@ -1,30 +1,27 @@
 import React from 'react';
 import { fromJS } from 'immutable';
 import ImmutableComponent from './components/common';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store/index';
 import logo from './logo.svg';
-import Header from './components/Header';
 import './App.css';
+import Header from './components/Header';
+import Home from './pages/home/index';
+import Detail from './pages/detail/index';
 
 class App extends ImmutableComponent {
-  // 最外层数据依然是原生js对象?
-  // redux
-  state = {
-    title: fromJS([1,2,3,[]])  // '123' 本身是不可变的
-  }
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        title: fromJS([1,2,3,[]])
-      })
-    }, 3000)
-  }
   render() {
-    console.log('app render');
     return (
-      <div>
-        123456
-        <Header title={this.state.title} />
-      </div>
+      <Provider store={ store }>
+        <BrowserRouter>
+          {/* / */}
+          {/* /detail 包含关系 */}
+          <Header />
+          <Route path="/" component={ Home } exact />
+          <Route path="/detail" component={ Detail } />
+        </BrowserRouter>
+      </Provider>
     )
   }
 }
