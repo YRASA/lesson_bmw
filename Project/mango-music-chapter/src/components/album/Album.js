a9d039c4f15544914a2baf4145ed5ca47493ed89
Zzceaon <zzceaon@outlook.com>
2020-06-15 00:53:50 +0800

    mango-music-chapter/src/components/album/

---
+++ Project/mango-music-chapter/src/components/album/Album.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './album.styl';  // webpack
+
+class Album extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>
+        Album
+      </div>
+     );
+  }
+}
+ 
+export default Album;
