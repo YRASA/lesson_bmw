175713c6320ef3009e7a6956d20f07a8a902fd80
Zzceaon <zzceaon@outlook.com>
2020-06-14 20:59:36 +0800

    react-jianshu/src/store/

---
+++ React/react-jianshu/src/store/constants.js
@@ -0,0 +1,3 @@
+// home
+export const GET_HOME_LIST = 'GET_HOME_LIST';
+// detail
---
+++ React/react-jianshu/src/store/index.js
@@ -0,0 +1,20 @@
+// import { combineReducers } from 'redux-immutable';
+import { createStore,
+  combineReducers,
+  applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import HomeReducer from './reducers/home/index';
+
+// 多个 reducer 合并成一个
+const rootReducer = combineReducers({
+  home: HomeReducer
+})
+// rootReducer = { home: immutable }
+// rootReducer 整个 rootReducer 并不是 immutable 的
+// state = { title: fromJS() }
+// HomeReducer 里面数据 immutable
+// rootReducer 里面的数据 还是原生 js
+// 创建 store 只能接收到 一个 reducer
+// 所以创建之前合并一下
+
+export default createStore(rootReducer)
