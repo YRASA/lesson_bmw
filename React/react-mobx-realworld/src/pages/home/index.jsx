65ac3bfc42421b92e558dbe8d4e34dcbb95bf955
Zzceaon <zzceaon@outlook.com>
2020-06-17 12:07:46 +0800

    react-mobx.realworld

--- React/react-mobx-realworld/src/pages/home/index.jsx
+++ React/react-mobx-realworld/src/pages/home/index.jsx
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
+import { Pagination } from 'antd';
 
 // 想要哪个页面的数据 注入流
 @inject('articleStore')
 @observer
 class Home extends Component {
-  state = {  }
-  render() { 
+  componentDidMount () {
+    this.props.articleStore.getArticle('all');
+  }
+  handleChange = (page) => {
+    // 1 offset 0
+    // 2 offset 1
+    // 3 offset 2
+    this.props.articleStore.getArticle('all', page - 1);
+  }
+  render() {
+    const { total, LIMIT } = this.props.articleStore
+    console.log(total, LIMIT)
     return (
       <div>
         { this.props.articleStore.articles.all.length }
+        <Pagination
+        onChange={this.handleChange}
+        total={total}
+        pageSize={LIMIT}
+        defaultCurrent={1}/>
       </div>
     );
   }
 }
- 
+
+// export default inject('articleStore')(observer(Home));
 export default Home;
--- React/react-mobx-realworld/src/store/articleStore.js
+++ React/react-mobx-realworld/src/store/articleStore.js
@@ -5,16 +5,33 @@ const LIMIT = 10;
 class ArticleStore {
   // observable state 用 observable 修饰过的变量 变量被修改了 页面就会重新渲染
   // es @ 装饰器
+  LIMIT = LIMIT
+  // react state 响应式 VM
   @observable articles = {
-    all: [],
-    cars: []
+    all: []
   }
 
+  // getArticle = action((tag, offset = 0) => {
+  //   axios.get('/articles', {
+  //     params: {
+  //       tag: tag === 'all' ? null : tag,
+  //       offset,
+  //       limit: LIMIT
+  //     }
+  //   })
+  //   .then(res => {
+  //     // 修改 store
+  //     // this.setState()
+  //     this.articles[tag] = res.articles
+  //   })
+  // })
+  @observable total = 0;
+  // 繁杂的逻辑尽量写到store
   @action
   getArticle(tag, offset = 0) {
     axios.get('/articles', {
       params: {
-        tag,
+        tag: tag === 'all' ? null : tag,
         offset,
         limit: LIMIT
       }
@@ -23,6 +40,9 @@ class ArticleStore {
       // 修改 store
       // this.setState()
       this.articles[tag] = res.articles
+      this.total = res.articlesCount
     })
   }
 }
+
+export default new ArticleStore();
