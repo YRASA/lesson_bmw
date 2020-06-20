33a6336d9a691de55002a1a426399074e3727a51
Zzceaon <zzceaon@outlook.com>
2020-06-20 17:59:15 +0800

    修改头部模板

---
+++ React/demo/src/headerEdit.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: Zzceaon
+ * @Date: 2020-06-20 17:43:28
+ * @LastEditTime: 2020-06-20 17:58:21
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Course\React\demo\src\headerEdit.js
+ */ 
+
+/**
+ * @description: 
+ * @param {type} 
+ * @return: 
+ */ 
+function add(x, y) {
+  return x + y
+}
