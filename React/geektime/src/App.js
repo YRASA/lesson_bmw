c8b28c711ddb4126f6ad193cd778d4a1805ff516
Zzceaon <zzceaon@outlook.com>
2020-07-11 10:28:51 +0800

    GeekTime

---
+++ React/geektime/src/App.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: Zzceaon
+ * @Date: 2020-07-11 10:22:54
+ * @LastEditTime: 2020-07-11 10:24:34
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Course\React\geektime\src\App.js
+ */ 
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import GeekTime from './GeekTime';
+
+function App() {
+  return (
+    <div className="App">
+      <GeekTime />
+    </div>
+  );
+}
+
+export default App;
