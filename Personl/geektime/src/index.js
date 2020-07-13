64b88a128f49d479ff6f5018eba7aceda1ce421e
Zzceaon <zzceaon@outlook.com>
2020-07-13 22:05:44 +0800

    ...

--- Personl/geektime/src/index.js
+++ Personl/geektime/src/index.js
@@ -1,14 +1,17 @@
-/*
- * @Author: Zzceaon
- * @Date: 2020-07-12 21:33:19
- * @LastEditTime: 2020-07-12 21:50:30
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \Course\Personl\geektime\src\index.js
- */ 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Lesson from './Lesson';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Lesson />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
