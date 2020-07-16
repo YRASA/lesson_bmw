5f7d8991ff235126735b898a6455edc50b89b60f
Zzceaon <zzceaon@outlook.com>
2020-07-16 12:15:50 +0800

    mobile-rem

---
+++ React/mobile-rem/src/App.js
@@ -0,0 +1,26 @@
+/*
+ * @Author: Zzceaon
+ * @Date: 2020-07-16 11:51:49
+ * @LastEditTime: 2020-07-16 11:53:46
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Course\React\mobile-rem\src\App.js
+ */ 
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="header">
+        <h2>LOGO</h2>
+        <input type="text" placeholder="search"/>
+        <button>登陆</button>
+      </div>
+      <div className="app"></div>
+    </div>
+  );
+}
+
+export default App;
