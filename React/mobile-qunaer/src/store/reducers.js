0952e6d10977227daa4ad054fd4a329d354ef53b
Zzceaon <zzceaon@outlook.com>
2020-07-16 16:19:34 +0800

    去哪儿

--- React/mobile-qunaer/src/store/reducers.js
+++ React/mobile-qunaer/src/store/reducers.js
@@ -1,12 +1,21 @@
 /*
  * @Author: Zzceaon
  * @Date: 2020-07-15 15:47:14
- * @LastEditTime: 2020-07-15 16:27:29
+ * @LastEditTime: 2020-07-16 15:08:32
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \Course\React\mobile-qunaer\src\store\reducers.js
  */ 
 import { ACTION_SET_FROM, ACTION_SET_TO } from './action';
+
+// state {
+//   from: '北京',
+//   to: '上海',
+//   time: {
+//     start: ,
+//     end:
+//   }
+// }
 
 export default {
   from(state = '北京', action) {
