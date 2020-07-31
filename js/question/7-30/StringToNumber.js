6cc1a25866924feaf49c4be762dc0a471043a688
Zzceaon <zzceaon@outlook.com>
2020-07-31 13:29:07 +0800

    7-30-Question

---
+++ js/question/7-30/StringToNumber.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: Zzceaon
+ * @Date: 2020-07-30 16:25:19
+ * @LastEditTime: 2020-07-31 13:28:22
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Course\js\question\7-30\StringToNumber.js
+ */ 
++"456" => 456
+Number("456") => 456
+
+ASCII 0: 48
+"4".charCodeAt(0) - 48 => 4
+
+"456" => 456:
+("456".charCodeAt(0) - 48)*(10 ** 2) + ("456".charCodeAt(1) - 48)*(10 ** 1) + ("456".charCodeAt(2) - 48)*(10 ** 0)
